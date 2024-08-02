4ca89858f48217185099b473364755f97b411499
Eduardo Filho <eduardo54098466@aluno.ms.senac.br>
2024-08-01 21:12:18 -0400

    adicionando arquivo para views

---
+++ views.py
@@ -0,0 +1,12 @@
+import classe
+
+def menu_principal():
+    
+    print('1 - criar cão')
+    print('2 - criar humano')
+    print('3 - listar cão')
+    print('4 - listar humano')
+    print('5 - editar cão')
+    print('6 - editar humano')
+    print('7 - excluir cão')
+    print('8 - excluir humano')
